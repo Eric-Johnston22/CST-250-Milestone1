fc18bff7a93290aa1dea56f129f16f87535e854c
Eric-Johnston <eric.mjohnston@protonmail.com>
2022-11-07 01:33:09 -0500

    Add project files.

---
+++ GameClassLibrary/Board.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameClassLibrary
+{
+    public class Board
+    {
+        public int size { get; set; }
+        public Cell[,] grid { get; set; }
+        public int difficulty { get; set; }
+
+        // Board constructor
+        public Board(int size)
+        {
+            grid = new Cell[size, size];
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    grid[i, j] = new Cell();
+                }
+            }
+        }
+
+        // Determines which locations in the grid is a mine
+        public void setupLiveNeighbors(int difficulty)
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    int randNumber = rand.Next(1, difficulty);
+                    if(randNumber == 1)
+                    {
+                        grid[i, j].live = true;
+                    }
+                }
+            }
+        }
+
+        // Determines how many neighboring grids have mines (still needs work)
+        public void calculateLiveNeighbors()
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    int randNumber = rand.Next(0, 8);
+                    if (grid[i, j].live == false)
+                    {
+                        grid[i, j].liveNeighbors = randNumber;
+                    }
+                    else if (grid[i, j].live == true)
+                    {
+                        grid[i, j].liveNeighbors = 9;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using GameClassLibrary;
+
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        { 
+            printBoard();
+        }
+
+        static public void printBoard()
+        {
+            Board board = new Board(9);
+
+            board.setupLiveNeighbors(6);
+            board.calculateLiveNeighbors();
+            for (int i = 0; i < board.grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.grid.GetLength(1); j++)
+                {
+                    
+                    Console.Write(board.grid[i, j].liveNeighbors + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
