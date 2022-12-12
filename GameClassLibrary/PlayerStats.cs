27582c04b668324b3a01c2ac21ecdebfdcdc66fd
Eric-Johnston <eric.mjohnston@protonmail.com>
2022-12-12 01:32:28 -0500

    added PlayerStats class

---
+++ GameClassLibrary/PlayerStats.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameClassLibrary
+{
+    public class PlayerStats : IComparable<PlayerStats>
+    {
+        public int highScore { get; set; }
+
+
+        public int CompareTo(PlayerStats? other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
